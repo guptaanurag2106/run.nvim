98416c677cfca0e548ffc31811962df9ce184a98
guptaanurag2106 <2106.guptaanurag@gmail.com>
2025-04-07 15:16:50 +0530

    Add default commands and user input

--- lua/run/config.lua
+++ lua/run/config.lua
@@ -8,6 +8,38 @@ local defaults = {
         log_file = vim.fn.stdpath("cache") .. "/run.nvim.log"
     },
     default_actions = {
+        [".py"] = {
+            command = "python %f",
+            description = "Run python files (with default python interpreter)"
+        },
+        [".bash"] = {
+            command = "bash %f",
+            description = "Run bash files"
+        },
+        [".tar.gz"] = {
+            command = "tar -xvf %f",
+            description = "Extracts archive file"
+        },
+        ["exe"] = {
+            command = "%1",
+            description = "Runs the file (only one) if it is executable"
+        },
+        ["no_extension"] = {
+            command = "chmod +x %f",
+            description = "chmod +x on the file (if no extension and not executable)"
+        },
+        ["dir"] = {
+            command = "tar czvf %1.tar.gz %f",
+            description = "tar.gz on the folder or multiple folders"
+        },
+        ["multiple"] = {
+            command = "tar czvf %1.tar.gz %f",
+            description = "tar.gz all the files/folders (if mixture of file/folder types)"
+        },
+        ["default"] = {
+            command = "xdg-open %f",
+            description = "xdg-open the file"
+        },
     }
 }
 
--- lua/run/init.lua
+++ lua/run/init.lua
@@ -1,16 +1,16 @@
-local browsers = require("run.browsers")
+local _browsers = require("run.browsers")
 local config = require("run.config")
 
 local M = {}
 
 M.setup = function(opts)
     config.setup(opts)
-    browsers.set_current_browser(config.options.current_browser)
+    _browsers.set_current_browser(config.options.current_browser)
 
     if config.options.browsers then
         for browser_name, funcs in pairs(config.options.browsers) do
             for func_name, func in pairs(funcs) do
-                browsers.register(browser_name, func_name, func)
+                _browsers.register(browser_name, func_name, func)
             end
         end
     end
@@ -28,36 +28,74 @@ end, { desc = "Run `command` on directory open in browser" })
 
 M.runfile = function()
     local bufnr = vim.api.nvim_get_current_buf()
-    local ok, ret = pcall(M.get_current_file, bufnr)
+
+    local ok, curr_file = pcall(M.get_current_file, bufnr)
     if not ok then
         print("Cannot get current file. Please ensure you are in the file browser: " .. config.options.current_browser)
-        print(ret)
+        print(curr_file)
     end
-    ok, ret = pcall(M.get_current_dir, bufnr)
-    if not ok then
-        print("Cannot get current directory. Please ensure you are in the file browser: " ..
-            config.options.current_browser)
-        print(ret)
+
+    local type = M._get_selected_type({ curr_file })
+    local command = config.options.default_actions[type].command or "xdg-open %f"
+    local prompt = "Run " .. command
+
+    local input = vim.fn.input(prompt)
+    if not input or string.len(input) == 0 then
+        input = command
     end
+
+    print("Chosen " .. input)
+
+    -- Fill values for %f %1 etc. Add %f at end if no %.. found
+    input = M._fill_input(input, { curr_file })
+    -- Run `input`
 end
 
 M.rundir = function()
     local bufnr = vim.api.nvim_get_current_buf()
-    local ok, ret = pcall(M.get_current_dir, bufnr)
+    local ok, curr_dir = pcall(M.get_current_dir, bufnr)
     if not ok then
         print("Cannot get current directory. Please ensure you are in the file browser: " ..
             config.options.current_browser)
-        print(ret)
+        print(curr_dir)
     end
+
+    local type = "dir"
+    local command = config.options.default_actions[type].command or "tar czvf %1.tar.gz %f"
+    local prompt = "Run " .. command
+
+    local input = vim.fn.input(prompt)
+    if not input or string.len(input) == 0 then
+        input = command
+    end
+
+    -- Fill values for %f %1 etc. Add %f at end if no %.. found
+    input = M._fill_input(input, { curr_dir })
+
+    print("Chosen " .. input)
+    -- Run `input`
 end
 
 
+---Returns type for the file list. See `config.default_actions` for all possible types
+---@param file_list table
+---@return string
+M._get_selected_type = function(file_list)
+    return "default"
+end
+
+---Get path of selected file (1)
+---@param bufnr integer bufnr of the open browser
+---@return string|nil Path of the selected file
 M.get_current_file = function(bufnr)
-    print(browsers.get_current_file(bufnr))
+    return _browsers.get_current_file(bufnr)
 end
 
+---Get path of open folder in browser
+---@param bufnr integer bufnr of the open browser
+---@return string|nil Path of the open folder
 M.get_current_dir = function(bufnr)
-    print(browsers.get_current_dir(bufnr))
+    return _browsers.get_current_dir(bufnr)
 end
 
 M.get_line = function(bufnr)
